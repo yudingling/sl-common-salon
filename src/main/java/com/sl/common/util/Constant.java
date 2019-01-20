1ccf7657dbeffba41180862172c580f80f70995c
zuodan <yudingling@126.com>
2019-01-20 14:16:20 +0800

    update

--- src/main/java/com/sl/common/util/Constant.java
+++ src/main/java/com/sl/common/util/Constant.java
@@ -13,4 +13,10 @@ public class Constant {
 	 */
 	private static final String REDIS_PREFIX = "salon:";
 	public static final String REDIS_PREFIX_TOKEN = REDIS_PREFIX + "tk_";
+	
+	/**
+	 * distributed lock prefix
+	 */
+	private static final String LOCK_PREFIX = REDIS_PREFIX + "lock:";
+	public static final String LOCK_PREFIX_RESERVATION = LOCK_PREFIX + "resv_";
 }
