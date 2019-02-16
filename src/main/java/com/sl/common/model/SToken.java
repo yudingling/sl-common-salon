d0f966f52586f8c5903564b2ebb21ae5941c0de1
zuodan <yudingling@126.com>
2019-02-16 15:14:46 +0800

    update

--- src/main/java/com/sl/common/model/SToken.java
+++ src/main/java/com/sl/common/model/SToken.java
@@ -8,6 +8,7 @@ public class SToken implements Serializable {
 	private String brandId;
 	private Long userId;
 	private String weChatSessionKey;
+	private String weChatOpenId;
 	private UserType userType;
 	
 	public String getBrandId() {
@@ -28,6 +29,12 @@ public class SToken implements Serializable {
 	public void setWeChatSessionKey(String weChatSessionKey) {
 		this.weChatSessionKey = weChatSessionKey;
 	}
+	public String getWeChatOpenId() {
+		return weChatOpenId;
+	}
+	public void setWeChatOpenId(String weChatOpenId) {
+		this.weChatOpenId = weChatOpenId;
+	}
 	public UserType getUserType() {
 		return userType;
 	}
@@ -46,11 +53,12 @@ public class SToken implements Serializable {
 		this.userType = userType;
 	}
 	
-	public SToken(UserForToken token, String weChatSessionKey) {
+	public SToken(UserForToken token, String weChatSessionKey, String weChatOpenId) {
 		super();
 		this.brandId = token.getBdId();
 		this.userId = token.getuId();
 		this.weChatSessionKey = weChatSessionKey;
+		this.weChatOpenId = weChatOpenId;
 		this.userType = UserType.valueOf(token.getRoleId());
 	}
 }
